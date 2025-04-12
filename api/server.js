107c46a6e49f4c0aa27e7e79cfccebb30d870dc2
Osmar Montesinos <osmarmontes50@gmail.com>
2025-04-11 20:07:00 -0500

    fixed cors errors

--- api/server.js
+++ api/server.js
@@ -26,12 +26,17 @@ const rules = jsonServerAuth.rewriter(JSON.parse(fs.readFileSync(path.join(__dir
 server.use(middlewares);
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Methods', '*');
+    const allowedOrigins = ['http://localhost:4173', 'http://localhost:5173', 'http://localhost:3000'];
+    const origin = req.headers.origin;
+    
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Expose-Headers', 'X-Total-Count, X-Total-Pages');
     
-    // Handle OPTIONS method
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
     }
