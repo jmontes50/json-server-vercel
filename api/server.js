2cc20a38da7c8486d62b7cbbad9f4d82cc34a094
Osmar Montesinos <osmarmontes50@gmail.com>
2025-04-11 20:02:02 -0500

    fixed cors errors

--- api/server.js
+++ api/server.js
@@ -26,6 +26,19 @@ const rules = jsonServerAuth.rewriter(JSON.parse(fs.readFileSync(path.join(__dir
 server.use(middlewares);
 
 server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Expose-Headers', 'X-Total-Count, X-Total-Pages');
+    
+    // Handle OPTIONS method
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+server.use((req, res, next) => {
     res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
     res.setHeader('Access-Control-Expose-Headers', 'X-Total-Pages');
     next();
